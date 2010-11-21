f39372033392f5180d3764ff62c8f4fe89ff316e
Daniel Abrahao <daniel@preface.com.br>
2010-11-20 23:08:10 -0200

    Adding JsonView to burocrata plugin.

---
+++ src/cake/app/plugins/burocrata/views/json.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Methods for displaying presentation data in JSON for passing data via JS.
+ *
+ * PHP versions 4 and 5
+ *
+ * @copyright 
+ * @link      
+ * @package   
+ * @subpackage
+ * @since     
+ * @license   
+ */
+ 
+ 
+/**
+ * Methods for displaying presentation data in JSON for passing data via JS.
+ */
+class JsonView extends View
+{
+	public $jsonVars;
+	
+	function __construct(&$controller)
+	{
+		$this->jsonVars = array();
+		$this->__passedVars[] = 'jsonVars';
+		
+		parent::__construct($controller);
+	}
+	
+	
+/**
+ * Overloads the default render action
+ *
+ * @param string $action Name of action to render for
+ * @param string $layout Layout to use
+ * @param string $file Custom filename for view
+ * @return string Rendered Element
+ * @access public
+ */
+	function render($action = null, $layout = null, $file = null)
+	{
+		if(Configure::read() > 1)
+			Configure::write('debug', 1);
+		header('Content-type: application/json');
+		
+		$loaded;
+		if(!isset($this->Js))
+			$this->_loadHelpers($loaded, array('Js'));
+		$this->Js = $loaded['Js'];
+		
+		if(isset($this->viewVars['jsonVars'])) {
+			return $this->Js->object($this->viewVars['jsonVars']);
+		} elseif(isset($this->jsonVars)) {
+			return $this->Js->object($this->jsonVars);
+		} else {
+			if($this->layout == null)
+				$layout = 'ajax';
+			return parent::render($action, $layout, $file);
+		}
+	}
+}
