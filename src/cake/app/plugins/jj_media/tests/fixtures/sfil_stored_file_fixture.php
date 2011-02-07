4cd54aa62b0fee03da43f5db12a57c19d0c5fae6
Daniel Abrahao <daniel@preface.com.br>
2011-02-07 18:47:35 -0200

    Baking of SfilStoredFile model

---
+++ src/cake/app/plugins/jj_media/tests/fixtures/sfil_stored_file_fixture.php
@@ -0,0 +1,46 @@
+<?php
+/* SfilStoredFile Fixture generated on: 2011-02-07 17:02:35 : 1297108655 */
+class SfilStoredFileFixture extends CakeTestFixture {
+	var $name = 'SfilStoredFile';
+
+	var $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'checksum' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'dirname' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'basename' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'original_filename' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'mime_type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'size' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'width' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'height' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'original_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'transformation_version' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'transformation' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'data' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+	var $records = array(
+		array(
+			'id' => 1,
+			'checksum' => 'Lorem ipsum dolor sit amet',
+			'dirname' => 'Lorem ipsum dolor sit amet',
+			'basename' => 'Lorem ipsum dolor sit amet',
+			'original_filename' => 'Lorem ipsum dolor sit amet',
+			'mime_type' => 'Lorem ipsum dolor sit amet',
+			'size' => 1,
+			'width' => 1,
+			'height' => 1,
+			'original_id' => 1,
+			'transformation_version' => 1,
+			'transformation' => 'Lorem ipsum dolor sit amet',
+			'data' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'created' => '2011-02-07 17:57:35',
+			'modified' => '2011-02-07 17:57:35'
+		),
+	);
+}
+?>
