6fad7327f1741f75fa32285a971d141b5c7b121f
lucas vignoli <lucas@preface.com.br>
2011-02-19 17:00:21 -0200

    jj_console: added functionality

--- src/cake/app/plugins/jj_utils/vendors/shells/jj_console.php
+++ src/cake/app/plugins/jj_utils/vendors/shells/jj_console.php
@@ -2,18 +2,63 @@
 
 class JjConsoleShell extends Shell {
 
+	var $issuedCommands = array();
+
 	function main()
 	{
+		$command = '';
 	
 		while (1)
 		{			
-			$answer = $this->in(__('JjConsoleShell::main What is the command to execute? Say "quit" to exit.', true));
+			$answer = $this->in(__('JjConsoleShell::main What is the command to execute? Say "quit" to exit, "help" to achieve help.', true));
 			
-			if (in_array($answer,array('quit','exit','q')))
-				return;
+			if (substr($answer, -1) == "\\")
+			{
+				$command .= substr($answer,0,strlen($answer)-1) . "\n";
+				do 
+				{
+					$answer = $this->in('');
+					if (substr($answer, -1) == "\\")
+					{
+						$command .= substr($answer,0,strlen($answer)-1) . "\n";
+					}
+					else
+					{
+						$command .= $answer;
+					}
+				}
+				while (substr($answer, -1) == "\\");
+			}
+			else
+				$command .= $answer;
+			
+			switch ($command)
+			{
+				case 'help':
+					$this->out(__('This is the help', true));
+				break;
 				
-			eval($answer);
+				case 'quit':
+				case 'exit':
+				case 'q':
+					return;
+				break;
+				
+				default:
+					$issuedCommands[] = $command;
+					if (substr($command,0,1) == ":")
+					{
+						$command = substr($command,1);
+						eval ('print_r(' . $command . ');');
+					}
+					else
+					{
+						eval ($command . ';');
+					}
+					
+			}
 			$this->out("\n");
+			$command = '';	
 		}
 	}
 }
