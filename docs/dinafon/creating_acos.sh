9f83a12e8b95b7c3fa326ea111a7804456110b2e
lucas vignoli <lucas@preface.com.br>
2011-02-22 16:50:18 -0300

    Dinafon's users, groups, acos, aros, all set!

---
+++ docs/dinafon/creating_acos.sh
@@ -0,0 +1,24 @@
+#Creating the acos tree
+#The tree to be created:
+#
+#public_page
+#backstage_area
+#	new_news
+#page_sections_test
+#	section_one
+#	section_two
+#		section_two_one
+
+
+cake acl create aco root all_pages
+#
+cake acl create aco all_pages public_page
+#
+cake acl create aco all_pages backstage_area
+cake acl create aco backstage_area new_news
+#
+cake acl create aco all_pages page_sections_test
+cake acl create aco page_sections_test section1
+cake acl create aco page_sections_test section2
+cake acl create aco section2 section21
+
