1a2e8b1f7f7d575eb7d595afb48c440e2541f7dc
lucas vignoli <lucas@preface.com.br>
2011-03-03 15:21:06 -0300

    Updating Dinafon SQL model and permission script.

--- docs/dinafon/4 - giving_and_taking_permissions.sh
+++ docs/dinafon/4 - giving_and_taking_permissions.sh
@@ -1,9 +1,9 @@
 #granting permission
 
-cake acl grant all_users public_page all
-cake acl grant editors backstage_area all
-cake acl grant redactors backstage_area read 
-cake acl grant redactors backstage_area edit 
-cake acl grant redactors backstage_area create
-cake acl grant techies backstage_area all
-cake acl grant superusers all_pages all
+./cake acl grant all_users public_page all
+./cake acl grant editors backstage_area all
+./cake acl grant redactors backstage_area read 
+./cake acl grant redactors backstage_area edit 
+./cake acl grant redactors backstage_area create
+./cake acl grant techies backstage_area all
+./cake acl grant superusers all_pages all
