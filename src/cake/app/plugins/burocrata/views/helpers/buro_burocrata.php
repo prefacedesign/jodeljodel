prefacedesign/jodeljodel

commit d297dade2fc2b8f4d8f3d2396b580b2f79e27e83

Starting to implement the many_children input
Now OfficeBoy inserts the generated scripts automagically on <head> and no more on <body>
Created a simple way to debug Ajax callbacks

src/cake/app/plugins/burocrata/views/helpers/buro_burocrata.php
@@ -28,6 +28,14 @@ class BuroBurocrataHelper extends XmlTagHelper
 	protected static $defaultObjectClass = 'buro';
 
 
+	public function beforeRender()
+	{
+		parent::beforeRender();
+		if (Configure::read() > 0)
+			$this->Html->css('/burocrata/css/ajax_dump', 'stylesheet', array('inline' => false));
+	}
+
+
 /**
  * An alias method for the View::element method that encloses the Jodel conventions
  *

src/cake/app/plugins/burocrata/webroot/js/burocrata.js
@@ -365,14 +365,17 @@ var BuroAjax = Class.create(BuroCallbackable, {
 	{
 		this.addCallbacks(callbacks);
 		
-		var ajax_options = {};
+		this.url = url;
 		
-		ajax_options.parameters = options.parameters;
-		ajax_options.onComplete = this.requestOnComplete.bind(this);
-		ajax_options.onSuccess = this.requestOnSuccess.bind(this);
-		ajax_options.onFailure = this.requestOnFailure.bind(this);
+		this.ajax_options = {};
+		this.ajax_options.parameters = options.parameters;
+		this.ajax_options.onComplete = this.requestOnComplete.bind(this);
+		this.ajax_options.onSuccess = this.requestOnSuccess.bind(this);
+		this.ajax_options.onFailure = this.requestOnFailure.bind(this);
+		
 		this.trigger('onStart');
-		new Ajax.Request(url, ajax_options);
+		
+		new Ajax.Request(this.url, this.ajax_options);
 	},
 	requestOnComplete: function (response) {
 		this.trigger('onComplete', response);
@@ -383,14 +386,17 @@ var BuroAjax = Class.create(BuroCallbackable, {
 		if(response.responseJSON) json = response.responseJSON;
 		
 		var headers = response.getAllHeaders();
-		if(!headers)
+		if(!headers) {
 			this.trigger('onFailure', response); // No server response
-		else if(!json)
+		} else if(!json) {
 			this.trigger('onError', E_NOT_JSON);
-		else if (json.error != false)
+			if (debug != 0)
+				this.dumpResquest(response);
+		} else if (json.error != false) {
 			this.trigger('onError', E_JSON, json.error);
-		else
+		} else {
 			this.trigger('onSuccess', response, json);
+		}
 	},
 	requestOnFailure: function(response)
 	{
@@ -406,6 +412,27 @@ var BuroAjax = Class.create(BuroCallbackable, {
 			default:
 				this.trigger('onFailure', response); // Page not found
 		}
+	},
+	dumpResquest: function(response)
+	{
+		var div = new Element('div', {className: 'dump_ajax'})
+			.insert(new Element('div', {className: 'dump_config'})
+				.insert('<h1>Config call</h1>')
+				.insert(new Element('pre')
+					.insert('URL: '+this.url+'<br>')
+					.insert(Object.toJSON(this.ajax_options))))
+			.insert(new Element('div', {className: 'dump_headers'})
+				.insert('<h1>Response headers</h1>')
+				.insert(new Element('pre').update(response.getAllHeaders())))
+			.insert(new Element('div', {className: 'dump_content'})
+				.insert('<h1>Response complete code</h1>')
+				.insert(new Element('pre').update(response.responseText.unfilterJSON().escapeHTML())))
+			.insert(new Element('div', {className: 'dump_code'})
+				.insert('<h1>Response content</h1>')
+				.insert(response.responseText/* .replace(/\{"\w+":.*\}/, '') */))
+		
+		div.observe('dblclick', function(ev){ev.findElement('div.dump_ajax').remove(); ev.stop();});
+		document.body.insert({top: div});
 	}
 });
 

src/cake/app/plugins/burocrata/views/helpers/buro_office_boy.php
@@ -14,17 +14,17 @@ class BuroOfficeBoyHelper extends AppHelper
 /**
  * Other helpers used by OfficeBoy
  * 
- * @var array
  * @access public
+ * @var array
  */
 	public $helpers = array('Html', 'Ajax', 'Js' => 'prototype');
 
 
 /**
- * Callbacks tamplate
+ * Callbacks template
  *
- * @var array
  * @access protected
+ * @var array
  */
 	protected $callbacks = array(
 		'form' => array(
@@ -67,6 +67,39 @@ class BuroOfficeBoyHelper extends AppHelper
 			'onRestart' => 'function(){%s}'
 		)
 	);
+
+/**
+ * To avoid include various scripts
+ * 
+ * @access protected
+ * @var boolean
+ */
+	protected $scripts_linked = false;
+
+/**
+ * An array of all created scripts. If is not a Ajax call, then it
+ * is used to print all script in one single blok on <head>
+ * 
+ * @access protected
+ * @var array
+ */
+	protected $scripts = array();
+
+
+/**
+ * afterRender callback used for print automagically all created scripts on HTML <head>
+ * 
+ * @access public
+ */
+	public function afterRender()
+	{
+		$View = ClassRegistry::getObject('view');
+		if ($View && !$this->Ajax->isAjax())
+		{
+			$view =& ClassRegistry::getObject('view');
+			$view->addScript($this->Html->scriptBlock($this->Js->domReady(implode("\n", $this->scripts))));
+		}
+	}
 
 
 /**
@@ -249,13 +282,14 @@ class BuroOfficeBoyHelper extends AppHelper
  *
  * @access public
  * @param string $script The script that will be appended
- * @return string The pice of code ready for HTML
+ * @return string|void The pice of code ready for HTML or nothing (if the script was put on buffer)
  */
 	public function addHtmlEmbScript($script)
 	{
-		if(!$this->Ajax->isAjax())
-			$script = $this->Js->domReady($script);
-		return $this->Html->scriptBlock($script);
+		if($this->Ajax->isAjax())
+			return $this->Html->scriptBlock($script);
+		else
+			$this->scripts[] = $script;
 	}
 
 
@@ -466,9 +500,14 @@ class BuroOfficeBoyHelper extends AppHelper
  */
 	protected function _includeScripts()
 	{
+		if ($this->scripts_linked)
+			return;
+		$this->scripts_linked = true;
+		
 		$this->Html->script('prototype', array('inline' => false));
 		$this->Html->script('effects', array('inline' => false));
 		$this->Html->script('controls', array('inline' => false));
 		$this->Html->script('/burocrata/js/burocrata.js', array('inline' => false));
+		$this->Html->scriptBlock('var debug = ' . Configure::read() . ';', array('inline' => false));
 	}
 }

src/cake/app/plugins/burocrata/webroot/css/ajax_dump.css
@@ -1,0 +1,16 @@
+.dump_ajax {
+    background: none repeat scroll 0 0 wheat;
+    border: 1px solid;
+    margin: 20px auto;
+    padding: 10px;
+    width: 956px;
+}
+.dump_ajax h1 {
+    border-bottom: 1px solid black;
+    margin: 20px 0 5px;
+    padding-bottom: 5px;
+}
+.dump_ajax > div {
+    overflow: auto;
+	padding-bottom: 10px;
+}

commit 10ec3e4da47305c1cf6b98fddd39d242720a48b9

Fixing a bug introduced in d297dade2fc2b8f4d8f3d2396b580b2f79e27e83
Now, if there a form js object doesnt find a form, it stops itself.

src/cake/app/plugins/burocrata/views/helpers/buro_burocrata.php
@@ -246,6 +246,7 @@ class BuroBurocrataHelper extends XmlTagHelper
  * @param  array $htmlAttributes Controls the HTML parameters
  * @param  array $options 
  * @return string The HTML well formated
+ * @todo Fix `type` parameter implementation
  */
 	public function sform($htmlAttributes = array(), $options = array())
 	{
@@ -285,7 +286,8 @@ class BuroBurocrataHelper extends XmlTagHelper
 		$this->_addFormAttribute('modelAlias', $this->modelAlias);
 		$this->_addFormAttribute('data', $options['data'] ? $options['data'] : $View->data);
 		
-		$this->Form->create($this->modelAlias, array('url' => $options['url']));
+		if ($this->modelAlias)
+			$this->Form->create($this->modelAlias, array('url' => $options['url']));
 		
 		$out = $this->Bl->sdiv($htmlAttributes);
 		if ($options['writeForm'] == true)
@@ -507,12 +509,16 @@ class BuroBurocrataHelper extends XmlTagHelper
 		$out .= $this->BuroOfficeBoy->newForm($this->_readFormAttributes());
 		
 		array_pop($this->_nestedForm);
-		$this->Form->end();
+		if ($this->modelAlias)
+			$this->Form->end();
 		
 		if (!empty($this->_nestedForm))
 		{
 			$options = $this->_readFormAttributes();
-			$this->Form->create($options['modelAlias'], array('url' => $options['url']));
+			$this->modelAlias = $options['modelAlias'];
+			$this->modelPlugin = $options['modelPlugin'];
+			if ($this->modelAlias)
+				$this->Form->create($this->modelAlias, array('url' => $options['url']));
 		}
 		
 		return $out;
@@ -2039,7 +2045,7 @@ class BuroBurocrataHelper extends XmlTagHelper
 		
 		$out = '';
 		
-		$this->sform(array(), array('model' => 'Textile'));
+		$this->sform(array(), array('url' => ''));
 		$out .= $this->Bl->sdiv(array('id' => 'div' . $gen_options['baseID']));
 			$out .= $this->inputLayoutScheme();
 			$out .= $this->input(array('value' => $packed, 'name' => $this->internalParam('data')), array('type' => 'hidden'));
@@ -2244,14 +2250,18 @@ class BuroBurocrataHelper extends XmlTagHelper
 				);
 				$popup_method = '_popupTextile'.Inflector::camelize($button);
 				if (method_exists($this, $popup_method))
-					$this->{$popup_method}(${"{$button}_id"}, $options);
+					$out .= $this->{$popup_method}(${"{$button}_id"}, $options);
 			}
 		}
 		
 		// Preview button, that has a special option
 		if ($config_options['allow_preview'])
 		{
-			$out .= $this->Bl->a(array('href' => '', 'class' => 'link_button buro_textile prev_textile', 'id' => $lprev_id), array(), __('Burocrata::inputTextile - Preview', true));
+			$out .= $this->Bl->a(
+				array('href' => '', 'class' => 'link_button buro_textile prev_textile', 'id' => $lprev_id), 
+				array(), 
+				__('Burocrata::inputTextile - Preview', true)
+			);
 			$out .= $this->_popupTextilePreview($prev_id, $options);
 		}
 		
@@ -2306,10 +2316,10 @@ class BuroBurocrataHelper extends XmlTagHelper
 		$popup_config['content'] = '';
 		$popup_config['content'] .= $this->Bl->pDry($popup_link_txt['instructions']);
 		
-		$popup_config['content'] .= $this->sform(array(), array('url' => ''));
+		$this->sform(array(), array('url' => ''));
 		$popup_config['content'] .= $this->input(array('id' => $itlink), array('container' => false, 'required' => true, 'label' => $popup_link_txt['label_text']));
 		$popup_config['content'] .= $this->input(array('id' => $iulink), array('container' => false, 'required' => true, 'label' => $popup_link_txt['label_link']));
-		$popup_config['content'] .= $this->eform();
+		$this->eform();
 		
 		return $this->Popup->popup($id, $popup_config);
 	}

src/cake/app/plugins/burocrata/webroot/js/burocrata.js
@@ -145,7 +145,10 @@ var BuroForm = Class.create(BuroCallbackable, {
 		{
 			this.id_base = arguments[1];
 			this.form = $('frm' + this.id_base);
-
+			
+			if (!this.form)
+				return;
+			
 			this.form.lock = this.lockForm.bind(this);
 			this.form.unlock = this.unlockForm.bind(this);
 			this.form.observe('keypress', this.keyPress.bind(this));

src/cake/app/plugins/burocrata_user/views/exemplos/index.ctp
@@ -1,25 +1,5 @@
 <?php
-
 echo $this->Bl->sbox(array(),array('size' => array('M' => 7, 'g' => -1)));
-	
-	// $popups = array('success', 'error', 'notice', 'form');
-	
-	// $content = array_flip($popups);
-	
-	// foreach ($popups as $popup_type)
-	// {
-		// echo $this->Popup->popup($popup_type, array(
-			// 'type' => $popup_type,
-			// 'content' => $content[$popup_type],
-			// 'title' => 'Titulo do '.$popup_type
-		// ));
-		// echo $this->Bl->a(array('href' => '', 'onclick' => "showPopup('$popup_type'); return false;"), array(), $popup_type);
-		// echo $this->Bl->br();
-		// echo $this->Bl->br();
-	// }
-
-	
-	// echo $this->Buro->insertForm('BurocrataUser.Galery', 'teste');	
 	
 	echo $this->Buro->sform(array('class' => 'azul'), // Parâmetros HTML
 		array(
@@ -70,7 +50,7 @@ echo $this->Bl->sbox(array(),array('size' => array('M' => 7, 'g' => -1)));
 				'fieldName' => 'about',
 				'type' => 'textile',
 				'options' => array(
-					'enabled_buttons' => array('bold', 'italic')
+					'enabled_buttons' => array('bold', 'italic', 'link')
 				)
 			)
 		);
@@ -81,7 +61,7 @@ echo $this->Bl->sbox(array(),array('size' => array('M' => 7, 'g' => -1)));
 				'type' => 'relational',
 				'label' => 'Owner of this gallery',
 				'options' => array(
-					'type' => 'unitary_autocomplete', // Former belongsTo, that now is also hasOne
+					'type' => 'unitary_autocomplete',
 					'model' => 'BurocrataUser.Person'
 				)
 			)