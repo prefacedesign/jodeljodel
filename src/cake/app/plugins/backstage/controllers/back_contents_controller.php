8ffe82a39f3ca24441955b8ef297787977f56708
Daniel Abrahao <daniel@preface.com.br>
2011-08-05 19:33:16 -0400

    Some improves on BackContentsController and documentation.

--- src/cake/app/plugins/backstage/controllers/back_contents_controller.php
+++ src/cake/app/plugins/backstage/controllers/back_contents_controller.php
@@ -1,27 +1,54 @@
 <?php
-
+/**
+ * SOME FILE INFORMATIONS
+ * 
+ * @package
+ * @subpackage
+ */
+ 
 App::import('Core','ModelBehavior');
 App::import('Behavior','Status.Status');
 
-
+/**
+ * SOME CLASS INFORMATIONS
+ * 
+ * @package
+ * @subpackage
+ */
 class BackContentsController extends BackstageAppController
 {
+/**
+ * Name for this controller
+ * 
+ * @access public
+ * @var string
+ */
     var $name = 'BackContents';
+
+/**
+ * List of Models to load on construct. Defaults to none.
+ * 
+ * @access public
+ * @var array
+ */
     var $uses = array();
 	
-	/** Action to edit/create a model row. Model must have implemented
-	 *  createEmpty (to create a new empty row) and. findBurocrata (to retrieve
-	 *  the data needed for the form). The plugin must have
-	 *  the element model_name with 'type' => array('burocrata','form').
-	 *
-	 *  @access	public
-	 *  @param	string 	$contentPlugin	The plugin name in underscore format.
-	 *	@param	string 	$modelName	The model name to be edited, in underscore format.
-	 *	@param	mixed	$id	The id of the row to be edited. If "null" it means that a new will be created.
-     */
-	
-    function edit($contentPlugin, $modelName, $id = null)
+/** 
+ * Action to edit/create a model row. Model must have implemented
+ * createEmpty (to create a new empty row) and. findBurocrata (to retrieve
+ * the data needed for the form). The plugin must have
+ * the element model_name with 'type' => array('burocrata','form').
+ *
+ * @access public
+ * @param string $contentPlugin The plugin name in underscore format.
+ * @param string $modelName The model name to be edited, in underscore format.
+ * @param mixed $id The id of the row to be edited. If "null" it means that a new will be created.
+ */
+    function edit($contentPlugin = false, $modelName = false, $id = null)
     {
+		if (empty($contentPlugin) || empty($modelName))
+			$this->cakeError('error404');
+		
         $fullModelName = Inflector::camelize($contentPlugin) . '.' . Inflector::camelize($modelName);
         $Model =& ClassRegistry::init($fullModelName);
         
@@ -29,18 +56,19 @@ class BackContentsController extends BackstageAppController
         {
             if (method_exists($Model, 'createEmpty'))
 			{
-                $this->data = $Model->createEmpty();
-				$this->redirect('edit/'. $contentPlugin .'/'. $modelName .'/'. $Model->id);
+                if ($Model->createEmpty())
+					$this->redirect(array($contentPlugin, $modelName, $Model->id));
+				elseif (Configure::read())
+					die ('Could not create an empty record.');
 			}
-            else
-                $this->data = null;
+			$this->redirect(array('plugin' => 'dashboard', 'controller' => 'dash_dashboard', 'action' => 'index'));
         }
         else
         {
-            if (!method_exists($Model, 'findBurocrata'))
-                $this->data = $Model->findById($id);
-            else
+            if (method_exists($Model, 'findBurocrata'))
                 $this->data = $Model->findBurocrata($id);
+            else
+                $this->data = $Model->findById($id);
         }
         
         $this->set('contentPlugin', $contentPlugin);
@@ -48,14 +76,14 @@ class BackContentsController extends BackstageAppController
         $this->set('fullModelName', $fullModelName);
     }
 	
-	/** To change the publishing status of a content. This action returns
-	 *  a JSON, with {"sucess":true} if the status change has success.
-	 *
-	 *	@access public
-	 *  @param	string 	$contentPlugin	The plugin name in underscore format.
-	 *	@param	string 	$modelName	The model name to be edited, in underscore format.
-     */
-	
+/** 
+ * To change the publishing status of a content. This action returns
+ * a JSON, with {"sucess":true} if the status change has success.
+ *
+ * @access public
+ * @param string $contentPlugin The plugin name in underscore format.
+ * @param string $modelName The model name to be edited, in underscore format.
+ */
 	function set_publishing_status($contentPlugin, $modelName, $id, $status)
 	{
 		$this->view = 'JjUtils.Json';
@@ -69,5 +97,3 @@ class BackContentsController extends BackstageAppController
 			$this->set('jsonVars', array('success' => false));
 	}
 }
-
-?>
