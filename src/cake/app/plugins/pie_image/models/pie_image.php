0c6b3059b7ab59dcea1fedb25b45719c4eebd937
Daniel Abrahao <daniel@preface.com.br>
2011-10-10 16:54:15 -0400

    PieImage removes the uploaded files if chenged or removed.

--- src/cake/app/plugins/pie_image/models/pie_image.php
+++ src/cake/app/plugins/pie_image/models/pie_image.php
@@ -1,5 +1,62 @@
 <?php
 class PieImage extends PieImageAppModel
 {
+/**
+ * Model name.
+ * 
+ * @var string
+ * @access public
+ */
 	var $name = 'PieImage';
+
+/**
+ * belongsTo relationship
+ * 
+ * @var array
+ * @access 
+ */
+	var $belongsTo = array(
+		'SfilStoredFile' => array(
+			'className' => 'JjMedia.SfilStoredFile',
+			'foreignKey' => 'file_id'
+		)
+	);
+
+/**
+ * Used to keep the current file_id and delete it, if changed.
+ * 
+ * @var false|string
+ * @access protected
+ */
+	protected $oldFileId = false;
+
+/**
+ * beforeSave callback
+ * 
+ * @access public
+ * @param array $options
+ * @return true Always return true.
+ */
+	function beforeSave($options)
+	{
+		$this->oldFileId = false;
+		if ($this->id)
+			$this->oldFileId = $this->field('file_id');
+		
+		return true;
+	}
+
+/**
+ * afterSave callback
+ * 
+ * If file_id changes, erases the old one.
+ * 
+ * @access public
+ * @param boolean $created If the register was created or was not.
+ */
+	function afterSave($created)
+	{
+		if (!$created && !empty($this->oldFileId) && $this->oldFileId != $this->data[$this->alias]['file_id'])
+			$this->SfilStoredFile->delete($this->oldFileId);
+	}
 }
