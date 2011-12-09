b38f49cac90f91a3f2e13b893ed8c698317f9dc0
Daniel Abrahao <daniel@preface.com.br>
2011-12-09 10:13:08 -0300

    SfilStoredFile now compatible with Media shell!

--- src/cake/app/plugins/jj_media/models/sfil_stored_file.php
+++ src/cake/app/plugins/jj_media/models/sfil_stored_file.php
@@ -12,8 +12,42 @@
 /**
  * Media enhanced plugin
  *
- * Media SfilStoredFile
+ * Media SfilStoredFile model uses the Media plugin behavior
  *
+ * Usage: is enough to this model pass the 'file' parameter to Model::save method.
+ * 
+ * {{{
+ * 	$this->SfilStoredFile->save($this->data); // When the data comes from one POST with input[type=file] on it.
+ * 	
+ * 	$data = array(
+ * 		'SfilStoredFile' => array('file' => '/full/path/to_file')
+ * 	);
+ * 	$this->SfilStoredFile->save($data); // When there is no data from POST and the file is already on server.
+ * }}}
+ * 
+ * The Media plugin have some Console scripts to handle filtering, and database/filtering
+ * consistency. For make this model to work with thoses scripts, nothing have to be done
+ * besides the model name specifying:
+ * 
+ * {{{
+ *  sudo -u www-data ./cake media sync JjMedia.SfilStoredFile -auto
+ * }}}
+ * 
+ * This script will delete any files that doesnt have an database record and will try
+ * to recover files from database record without file (by using checksum for each found
+ * file) or delete when recover doesnt work.
+ * 
+ * {{{
+ *  sudo -u www-data ./cake media make -model JjMedia.SfilStoredFile
+ * }}}
+ * 
+ * This script will rebuild all filtered files, so is a good pratice to remove the filter
+ * folder before run it.
+ * 
+ * Note that the scripts calls are using the `sudo -u www-data`, that way, the script
+ * runs under the apache user permissions and all created files will belongs to www-data
+ * 
+ * 
  * @package jj_media
  * @subpackage jj_media.models
  */
@@ -76,6 +110,7 @@ class SfilStoredFile extends JjMediaAppModel {
 			'baseDirectory' => MEDIA_TRANSFER,
 			'filterDirectory' => MEDIA_FILTER,
 			'createDirectory' => true,
+			'overwrite' => true,
 		),
 		'Media.Coupler' => array(
 			'baseDirectory' => MEDIA_TRANSFER
@@ -115,5 +150,16 @@ class SfilStoredFile extends JjMediaAppModel {
 			$this->data[$this->alias]['file'] = dirname($this->data[$this->alias]['file']) . DS . $newFilename;
 		
 		return $short . DS . $newFilename;
+	}
+	
+	function make($file)
+	{
+		$fileData = $this->findByBasename(basename($file));
+		if (!empty($fileData))
+		{
+			$this->createGeneratorConfigure($fileData[$this->alias]['transformation']);
+			return $this->Behaviors->dispatchMethod($this, 'make', array($file));
+		}
+		return false;
 	}
 }
