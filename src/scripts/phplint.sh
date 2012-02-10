9a23895c813094c26b9533789a9729bf3156ced4
Daniel Abrahao <daniel@preface.com.br>
2012-02-10 16:40:53 -0300

    phplint.sh translated

--- src/scripts/phplint.sh
+++ src/scripts/phplint.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-echo 'Coletando arquivos para averiguar'
+echo 'Collecting files for lint...'
 files=`find $1 -name *.php -or -name *.ctp -type f`
 count=`echo -n "$files" | wc -l`
 i=0
 
-echo "$count arquivos encontrados."; echo
+echo "...$count files found."; echo
 
 for file in $files
 do
 	result=`php -l $file` # | grep -v '^No syntax errors'`
-	i=$(($i+1))
 	porcent=$(($i*100/$count))
-	echo -n "Fazendo lint: $porcent%\r"
+	i=$(($i+1))
+	echo -n "Lint: $porcent% done\r"
 done
 
+echo 
 exit 0
