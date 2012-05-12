prefacedesign/jodeljodel

commit 137fd9fb72c97a61380fab94fd6d68ff53568428

Upload input does not require that form sets model parameter

src/cake/app/plugins/burocrata/views/helpers/buro_burocrata.php
@@ -2222,15 +2222,11 @@ class BuroBurocrataHelper extends XmlTagHelper
  */
 	protected function _uploadParams($options)
 	{
-		$modelAlias = $this->_readFormAttribute('modelAlias');
-		if (!$modelAlias)
-			trigger_error('Can\'t create a upload file that is not inside a buro form.', E_USER_WARNING);
-		
 		$file_input_options = array_filter($options);
 		unset($file_input_options['options']);
-		$file_input_options += array('fieldName' => $modelAlias . '.sfil_sored_file_id');
+		$file_input_options += array('fieldName' => 'sfil_sored_file_id');
 		if (strpos($file_input_options['fieldName'], '.') === false)
-			$file_input_options['fieldName'] = $modelAlias . '.' . $file_input_options['fieldName'];
+			$file_input_options['fieldName'] = $file_input_options['fieldName'];
 		
 		$defaults = array(
 			'baseID' => $this->baseID(),

commit 66e5120881685ef4468db5b67c03ce9f8e50d29d

Fixing a error introduced in 137fd9fb72c97a61380fab94fd6d68ff53568428

src/cake/app/plugins/burocrata/views/helpers/buro_burocrata.php
@@ -2209,9 +2209,10 @@ class BuroBurocrataHelper extends XmlTagHelper
 	{
 		$file_input_options = array_filter($options);
 		unset($file_input_options['options']);
+
 		$file_input_options += array('fieldName' => 'sfil_sored_file_id');
-		if (strpos($file_input_options['fieldName'], '.') === false)
-			$file_input_options['fieldName'] = $file_input_options['fieldName'];
+		if (strpos($file_input_options['fieldName'], '.') === false && $this->model())
+			$file_input_options['fieldName'] = $this->model() . '.' . $file_input_options['fieldName'];
 		
 		$defaults = array(
 			'baseID' => $this->baseID(),