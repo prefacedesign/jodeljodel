92521f66b9e9745e0290926de44020663ed9bd3e
Daniel Abrahao <daniel@preface.com.br>
2014-12-16 12:10:49 -0200

    New JjMedia set of options as configuration file

---
+++ src/cake/app/plugins/jj_media/config/options.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * When defined to a valid location (a mirror), the Media plugin will attempt to find a copy on the mirror
+ * to use it as original and create the filtered option
+ *
+ * $type string|false
+ */
+$config['JjMedia']['mirror'] = '';
